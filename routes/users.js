d5c6474d9c85005b715a6582b573608b19172aee
harringtonlyndsey <lyndsey.harrington@vodafone.com>
2023-11-10 16:23:38 +0000

    users added back in

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,22 @@ router.get("/", (req, res) => {
   res.send("hello");
 });
 
+router.get("/", async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
+});
+
+router.get("/:id", async (req, res) => {
+  const number = req.params.id;
+  const user = await User.findByPk(number);
+  res.json(user);
+});
+
+router.put("/:id", async (req, res) => {
+  const updatedUser = await User.update(req.body, {
+    where: { id: req.params.id },
+  });
+  res.json(updatedUser);
+});
+
 module.exports = router;
